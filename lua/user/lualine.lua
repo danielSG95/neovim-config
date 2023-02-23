4b147fa8049faa197fd84d0c36d6c944c683b9ff
danielensantos <dev1@nasca.com.gt>
2023-02-23 16:08:33 -0600

    adding personal nvim configuration

---
+++ lua/user/lualine.lua
@@ -0,0 +1,80 @@
+local custom_gruvbox = require'lualine.themes.horizon'
+local status_ok, lualine = pcall(require, "lualine")
+if not status_ok then
+  return
+end
+
+local hide_in_width = function()
+  return vim.fn.winwidth(0) > 80
+end
+
+local diagnostics = {
+  "diagnostics",
+  sources = { "nvim_lsp" },
+  sections = { 'error', 'warn', 'info', 'hint' }, 
+  symbols = { error = " ", warn = " ", info = "🛈 ", hint = " " },
+  colored = true,
+  always_visible = true,
+}
+
+local diff = {
+  "diff",
+  colored = true,
+  symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+  cond = hide_in_width,
+}
+
+local filetype = {
+  "filetype",
+  icons_enabled = true,
+}
+
+local location = {
+  "location",
+  padding = 0,
+}
+
+local spaces = function()
+  return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+end
+
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = custom_gruvbox,
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = true,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff' },
+    lualine_c = {'diagnostics'},
+    lualine_x = {'encoding', 'fileformat', 'filetype', 'filename'},
+    lualine_y = {'progress' },
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {'quickfix'}
+}
